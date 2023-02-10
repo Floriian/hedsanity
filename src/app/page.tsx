35c30d832fe65e92004e624bc60dbb8ddf5dc699
Floriian <lovasifloriian@outlook.hu>
2023-02-10 23:49:16 +0100

    index page improvments.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,41 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+import { groq } from "next-sanity";
+import HeroImage from "../components/HeroImage/HeroImage";
+import { client, imageUrlBuilder } from "../utils";
+import { HeroImageResponse } from "../types";
 
-const inter = Inter({ subsets: ['latin'] })
+const heroQuery = groq`*[_type == "heroimage"][0]`;
+export async function getHero() {
+  const hero = await client.fetch<HeroImageResponse>(heroQuery);
+  return hero;
+}
 
-export default function Home() {
+export default async function Home() {
+  const hero = await getHero();
+  const img = imageUrlBuilder(hero.heroimage.asset._ref).url();
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+    <div>
+      <HeroImage imageUrl={img} title={hero.herotitle} />
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((p, i) => (
+        <p key={i + 1}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis porro
+          omnis earum dolorum officia veniam error, recusandae voluptatum
+          tenetur libero iure deserunt natus, possimus rem hic id, dolorem
+          quaerat aliquam. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Nobis porro omnis earum dolorum officia veniam error, recusandae
+          voluptatum tenetur libero iure deserunt natus, possimus rem hic id,
+          dolorem quaerat aliquam. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Nobis porro omnis earum dolorum officia veniam
+          error, recusandae voluptatum tenetur libero iure deserunt natus,
+          possimus rem hic id, dolorem quaerat aliquam. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Nobis porro omnis earum dolorum
+          officia veniam error, recusandae voluptatum tenetur libero iure
+          deserunt natus, possimus rem hic id, dolorem quaerat aliquam. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Nobis porro omnis
+          earum dolorum officia veniam error, recusandae voluptatum tenetur
+          libero iure deserunt natus, possimus rem hic id, dolorem quaerat
+          aliquam.
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      ))}
+    </div>
+  );
 }
