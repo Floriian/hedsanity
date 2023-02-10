8f40888277d798a07569615737ec70ef2aaa462a
Floriian <lovasifloriian@outlook.hu>
2023-02-10 23:48:56 +0100

    create basic layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,22 @@
-import './globals.css'
+import Menu from "../components/layout/Menu";
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="hu">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <Menu />
+        <main>{children}</main>
+      </body>
     </html>
-  )
+  );
 }
